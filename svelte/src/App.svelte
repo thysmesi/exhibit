b48f8e3de3899f8556e3dd10469f29311d068fa6
thysmesi <77686907+thysmesi@users.noreply.github.com>
2021-07-22 12:46:59 -0600

    start lg

---
+++ svelte/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+
+	import a from './exhibit-pdf-kit'
+</script>
+
+<main>
+	<div id="options"></div>
+	<div id="present">
+		{a}
+	</div>
+</main>
+
+<style>
+	
+</style>
